4602bc77d7c2f770a8467b82566a04503950a6ea
SixthFactorConsutting <164723491+SixthFactor@users.noreply.github.com>
2024-03-24 15:55:13 +0530

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -15,7 +15,7 @@ st.set_page_config(
     layout="centered",  # Page layout option
 )
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = "AIzaSyC5jVGT9OHx4soEsliU60ByZsieobJPRms"  # Replace this with your real API key
 
 # Set up Google Gemini-Pro AI model
 gen_ai.configure(api_key=GOOGLE_API_KEY)
